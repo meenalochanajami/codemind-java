7d69c9a1af200438edb6834ffde898ff299678fc
meenalochanajami <119864795+meenalochanajami@users.noreply.github.com>
2023-08-16 23:32:36 +0530

    import java.util.Scanner; public class Operators {     public static void main(String args[])     {         Scanner sc= new Scanner(System.in);         double c,f;         c=sc.nextInt();         f=32+(c*1.8);         System.out.printf("%.2f",f);
        }
    }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Operators
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        double c,f;
+        c=sc.nextInt();
+        f=32+(c*1.8);
+        System.out.printf("%.2f",f);
+        
+    }
+}
