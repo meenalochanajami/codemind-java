2615c5070815371952a01b60db7a6e7113f6631f
meenalochanajami <119864795+meenalochanajami@users.noreply.github.com>
2023-08-16 23:24:42 +0530

    import java.util.Scanner; public class Operators {     public static void main(String args[])     {         Scanner sc= new Scanner(System.in);         double r,a;         r=sc.nextFloat();         a=3.14*r*r;         System.out.printf("%.2f",a);
        }
    }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Operators
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        double r,a;
+        r=sc.nextFloat();
+        a=3.14*r*r;
+        System.out.printf("%.2f",a);
+        
+    }
+}
