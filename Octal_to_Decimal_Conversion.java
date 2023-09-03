a3bf3bf3900a6acadc818f72a78c6d3c747116e7
meenalochanajami <119864795+meenalochanajami@users.noreply.github.com>
2023-09-03 09:53:55 +0530

    import java.util.*; public class pracice{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         String s=sc.next();         System.out.println(Integer.parseInt(s,8));     } }

---
+++ Octal_to_Decimal_Conversion.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class pracice{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        System.out.println(Integer.parseInt(s,8));
+    }
+}
